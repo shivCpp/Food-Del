7c838f7a3d5e4869070cbf4f6ef4939c93848bf7
Shivverma <162031756+shivCpp@users.noreply.github.com>
2026-06-18 03:50:13 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -9,7 +9,7 @@ const StoreContextProvider = (props) => {
     const [cartItems, setCartItems] = useState({});
 
 
-    const url = "http://localhost:4000"
+    const url = "https://food-del-backend-c606.onrender.com"
    
     
     const [token,setToken] = useState("");
